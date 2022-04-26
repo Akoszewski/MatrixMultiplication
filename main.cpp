26b4dfd08b264a4246f8ae9aeea2a08e99e6068c
ArekK <arkadiusz.koszewski@frey.pl>
2022-04-26 03:41:45 +0200

    Funkcja mnozaca in progress

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@
 
 const int maxVal = 9;
 
+class Matrix;
+
+int multiplyRow(const Matrix& mat, const Matrix& mat2, int leftMatrixColumn, int leftMatrixRow) // left matrix column and row
+{
+    int result = 0;
+    for (int i = 0; i < mat.getCols(); i++)
+    {
+        res += mat.data[leftMatrixRow][i] * mat2.data[i][leftMatrixRow]; //  tu jakos inaczej
+    }
+    return result;
+}
+
 class Matrix
 {
 public:
@@ -45,7 +57,15 @@ public:
     }
     Matrix operator*(const Matrix& mat2)
     {
-        return Matrix(2, 2);
+        Matrix res(2, 2);
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                res.data[i][j] = multiplyRow(*this, mat2, i, j);
+            }
+        }
+        return res;
     }
     std::vector<std::vector<int>> data;
 private:
